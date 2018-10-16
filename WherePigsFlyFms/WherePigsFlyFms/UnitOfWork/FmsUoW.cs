72b48c2f0adbaf9edee9001b7b850b805bce889c
Bcoker81 <Branden.coker@gmail.com>
2018-10-16 14:18:41 -0500

    added unit of work

---
+++ WherePigsFlyFms/WherePigsFlyFms/UnitOfWork/FmsUoW.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WherePigsFlyFms.Data;
+using WherePigsFlyFms.Models;
+using WherePigsFlyFms.Repository;
+
+namespace WherePigsFlyFms.UnitOfWork
+{
+    public class FmsUoW : IFmsUoW
+    {
+        private GenericRepository<Animals> _animalRepo;
+        private GenericRepository<MedicalRecords> _medicalRepo;
+
+        private FmsDbContext _entities;
+
+        public FmsUoW(FmsDbContext Entities)
+        {
+            _entities = Entities;
+        }
+
+        public GenericRepository<Animals> AnimalRepo {
+            get
+            {
+                if (_animalRepo == null)
+                {
+                    _animalRepo = new GenericRepository<Animals>(_entities);
+                }
+
+                return _animalRepo;
+            }
+        }
+
+        public GenericRepository<MedicalRecords> MedicalRepo
+        {
+            get
+            {
+                if (_medicalRepo == null)
+                {
+                    _medicalRepo = new GenericRepository<MedicalRecords>(_entities);
+                }
+
+                return _medicalRepo;
+            }
+        }
+
+        public void Commit()
+        {
+            _entities.SaveChanges();
+        }
+       
+    }
+}
---
+++ WherePigsFlyFms/WherePigsFlyFms/UnitOfWork/IFmsUoW.cs
@@ -0,0 +1,13 @@
+﻿using WherePigsFlyFms.Models;
+using WherePigsFlyFms.Repository;
+
+namespace WherePigsFlyFms.UnitOfWork
+{
+    public interface IFmsUoW
+    {
+        GenericRepository<Animals> AnimalRepo { get; }
+        GenericRepository<MedicalRecords> MedicalRepo { get; }
+
+        void Commit();
+    }
+}
