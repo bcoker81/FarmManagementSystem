0de5d495bccccd0ba9416f0e560088d33ca97b84
Branden <branden.coker@gmail.com>
2019-03-19 12:15:59 -0500

    added medical controller

---
+++ WherePigsFlyFms/WherePigsFlyFms/Controllers/MedicalRecordController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WherePigsFlyFms.ViewModels;
+
+namespace WherePigsFlyFms.Controllers
+{
+    public class MedicalRecordController : Controller
+    {
+        // GET: MedicalRecord
+        public ActionResult AddMedicalRecord(FarmViewModel model)
+        {
+            return null;
+        }
+    }
+}
